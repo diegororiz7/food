6463491e6759626fde24d94eebaddc97e74d8bdb
diegororiz7 <diegororiz7@gmail.com>
2021-10-28 20:25:50 -0300

    Alteração 28-10-2021 2

--- app/Views/Admin/Usuarios/form.php
+++ app/Views/Admin/Usuarios/form.php
@@ -1,24 +1,24 @@
 <div class="form-row">
     <div class="form-group col-md-4">
         <label for="nome">Nome</label>
-        <input type="text" class="form-control" id="nome" value="<?php echo old('nome', esc($usuario->nome)); ?>">
+        <input name="nome" type="text" class="form-control" id="nome" value="<?php echo old('nome', esc($usuario->nome)); ?>">
     </div>
 
     
     <div class="form-group col-md-2">
         <label for="cpf">CPF</label>
-        <input type="text" class="form-control cpf" id="cpf" value="<?php echo old('cpf', esc($usuario->cpf)); ?>">
+        <input name="cpf" type="text" class="form-control cpf" id="cpf" value="<?php echo old('cpf', esc($usuario->cpf)); ?>">
     </div>
 
     
     <div class="form-group col-md-3">
         <label for="telefone">Telefone</label>
-        <input type="text" class="form-control sp_celphones" id="telefone" value="<?php echo old('telefone', esc($usuario->telefone)); ?>">
+        <input name="rtelefone" type="text" class="form-control sp_celphones" id="telefone" value="<?php echo old('telefone', esc($usuario->telefone)); ?>">
     </div>
 
     <div class="form-group col-md-3">
         <label for="email">E-mail</label>
-        <input type="email" class="form-control" id="email" value="<?php echo old('email', esc($usuario->email)); ?>">
+        <input name="email" type="email" class="form-control" id="email" value="<?php echo old('email', esc($usuario->email)); ?>">
     </div>
 </div>
 
