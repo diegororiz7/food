94ec6175a5cce35a239026187f80ab4943dc779d
diegororiz7 <diegororiz7@gmail.com>
2021-11-06 08:22:24 -0300

    Commit 06-11-2021

---
+++ app/Controllers/Admin/Categorias.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Categorias extends BaseController{
+
+    private $categoriaModel;
+
+    public function __construct()
+    {
+
+       $this->categoriaModel = new \App\Models\CategoriaModel();  
+
+    }
+
+    public function index(){
+
+        $data = [
+            'titulo'     => 'Listando as categorias',
+            'categorias' => $this->categoriaModel->withDeleted(true)->paginate(10),
+            'pager'      => $this->categoriaModel->pager
+        ];
+
+        return view('Admin/Categorias/index', $data);
+
+    }
+
+    public function procurar(){
+
+        if(!$this->request->isAJAX()){
+
+            exit('Página não encontrada');
+
+        }
+
+        $categorias = $this->categoriaModel->procurar($this->request->getGet('term'));
+
+        $retorno  = [];
+
+        foreach ($categorias as $categoria){
+
+            $data['id'] = $categoria->id;
+            $data['value'] = $categoria->nome;
+
+            $retorno[] = $data;
+
+        }
+        
+        return $this->response->setJSON($retorno);
+
+    }
+}
---
+++ app/Models/CategoriaModel.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CategoriaModel extends Model
+{
+    protected $table                = 'categorias';
+    protected $returnType           = 'App\Entities\Categoria';
+    protected $useSoftDeletes       = true;
+    protected $allowedFields        = ['nome','slug','ativo'];
+
+    // Dates
+    protected $useTimestamps        = true;
+    protected $dateFormat           = 'datetime';
+    protected $createdField         = 'criado_em';
+    protected $updatedField         = 'atualizado_em';
+    protected $deletedField         = 'deletado_em';
+
+    //Validações
+    protected $validationRules    = [
+        'nome'             => 'required|min_length[2]|max_length[120]|is_unique[categorias.nome]',
+    ];
+    protected $validationMessages = [
+        'nome'        => [
+            'required'  => 'Desculpe, o campo nome é obrigatório!'
+        ],
+    ];
+
+    //Eventos callback
+    protected $beforeInsert = ['criaSlug'];
+
+    protected $beforeUpdate = ['criaSlug'];
+
+    protected function criaSlug(array $data){
+
+        if(isset($data['data'] ['nome'])){
+
+            $data['data']['slug'] = mb_url_title($data['data'] ['nome'], '-',TRUE);
+
+        }
+
+        return $data;
+
+    }
+
+    public function procurar($term){
+
+        if($term === null){
+            return[];
+        }
+
+        return $this->select('id, nome')
+                    ->like('nome', $term)
+                    ->withDeleted(true)
+                    ->get()
+                    ->getResult();    
+
+    }
+}
